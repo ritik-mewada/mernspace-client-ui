7646c2a87e9f8d31ade5314a582ffe21e5197f27
ritik-mewada <ritikmewada@gmail.com>
2025-09-13 15:11:18 -0400

    integrate redux toolkit

---
+++ src/lib/store/features/cart/cartSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface CartState {
+  value: number;
+}
+
+const initialState: CartState = { value: 0 };
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
+});
+
+export const { increment } = cartSlice.actions;
+export default cartSlice.reducer;
