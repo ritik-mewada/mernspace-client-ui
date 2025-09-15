af8e500d4f594652235560c5c7bf2956b36f19ab
ritik-mewada <ritikmewada@gmail.com>
2025-09-15 12:59:42 -0400

    render customer addresses

--- src/app/checkout/components/customerForm.tsx
+++ src/app/checkout/components/customerForm.tsx
@@ -100,24 +100,16 @@ const CustomerForm = () => {
                   defaultValue="option-one"
                   className="grid grid-cols-2 gap-6 mt-2"
                 >
-                  <Card className="p-6">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-one" id="option-one" />
-                      <Label htmlFor="option-one" className="leading-normal">
-                        123, ABC Street, Malad West, Mumbai, Maharashtra, India
-                        400064
-                      </Label>
-                    </div>
-                  </Card>
-                  <Card className="p-6">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="option-two" id="option-two" />
-                      <Label htmlFor="option-two" className="leading-normal">
-                        Flat No. 501, Sunshine Apartments, Andheri East, Mumbai,
-                        Maharashtra, India 400069
-                      </Label>
-                    </div>
-                  </Card>
+                  {customer?.addresses.map((address) => (
+                    <Card className="p-6" key={address.text}>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="option-one" id="option-one" />
+                        <Label htmlFor="option-one" className="leading-normal">
+                          {address.text}
+                        </Label>
+                      </div>
+                    </Card>
+                  ))}
                 </RadioGroup>
               </div>
             </div>
