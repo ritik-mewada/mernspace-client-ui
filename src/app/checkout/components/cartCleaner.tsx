6d514aa71b79012242f8a3d82b71044bc15c9f08
ritik-mewada <ritikmewada@gmail.com>
2026-01-19 19:16:20 -0500

    cleart cart on payment success

---
+++ src/app/checkout/components/cartCleaner.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { clearCart } from "@/lib/store/features/cart/cartSlice";
+import { useAppDispatch } from "@/lib/store/hooks/hooks";
+import React from "react";
+
+const CartCleaner = ({}) => {
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(clearCart());
+  }, []);
+
+  return null;
+};
+
+export default CartCleaner;
